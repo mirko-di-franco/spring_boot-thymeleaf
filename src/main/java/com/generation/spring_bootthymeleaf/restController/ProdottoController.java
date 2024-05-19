3b47a7e34410dfb544788122bd3ec11dab5b3530
Mirko Di Franco <120678275+mirko-di-franco@users.noreply.github.com>
2024-05-19 21:06:10 +0200

    completato modifica prodotto

--- src/main/java/com/generation/spring_bootthymeleaf/restController/ProdottoController.java
+++ src/main/java/com/generation/spring_bootthymeleaf/restController/ProdottoController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.generation.spring_bootthymeleaf.dto.ProdottoDTO;
@@ -55,6 +56,7 @@ public class ProdottoController {
 	}
 	
 	
+	//creo il prodotto e lo salvo su db
 	@PostMapping("/crea")
 	//@valid : esegue la validazione di prodottoDTO utilizzando le annotazioni di validazione presenti nella classe ProdottoDTO, come @NotNull, @Size
 	//@ModelAttribute: Spring cerca di legare i dati del form (o altri parametri della richiesta) a un'istanza di ProdottoDTO.
@@ -108,5 +110,94 @@ public class ProdottoController {
 		
 		return "redirect:/api/prodotti";
 	}
+	
+	//mi reinderizza alla pagina per modificare un prodotto
+	@GetMapping("/modifica")
+	public String paginaModificaProdotto(Model model, @RequestParam int idProdotto) {
+		
+		try {
+			//Il metodo ricercaProdotto mi ritorna un "Optional" però se metto .get non ho errori 
+			Prodotto prodotto = prodottoServ.ricercaProdotto(idProdotto).get();
+			model.addAttribute("prodotto", prodotto);
+			
+			
+			ProdottoDTO prodottoDTO = new ProdottoDTO();
+			
+			prodottoDTO.setNome(prodotto.getNome());
+			prodottoDTO.setMarca(prodotto.getMarca());
+			prodottoDTO.setCategoria(prodotto.getCategoria());
+			prodottoDTO.setPrezzo(prodotto.getPrezzo());
+			prodottoDTO.setDescrizione(prodotto.getDescrizione());
+			
+			model.addAttribute("prodottoDTO", prodottoDTO);
+			
+			
+		}catch(Exception e) {
+			System.out.println("Exception: "+ e.getMessage());
+			return "redirect:/api/prodotti";
+		}
+		
+		return "prodotti/modificaProdotto";
+	}
+	
 
+	
+	//modifico il prodotto
+	@PostMapping("/modifica")
+	public String modificaProdotto(Model model, @RequestParam int idProdotto, @Valid @ModelAttribute ProdottoDTO prodottoDTO, BindingResult result) {
+		
+		
+		try {
+			Prodotto prodotto = prodottoServ.ricercaProdotto(idProdotto).get();
+			model.addAttribute("prodotto", prodotto);
+			
+			if(result.hasErrors()) {
+				return "prodotti/modificaProdotto";
+			}
+			
+			if(!prodottoDTO.getImmagine().isEmpty()) {
+				//se non è vuoto allore provo ad eliminare la vecchia immagine
+				String percorsoCaricamento = "public/img/";
+				Path vecchiaImmagine = Paths.get(percorsoCaricamento + prodotto.getImmagine());
+				
+				try {
+					Files.delete(vecchiaImmagine);
+					
+					
+				}catch(Exception e) {
+					System.out.println("Exception"+ e.getMessage());
+				}
+				
+				
+				//salvo la nuova immagine
+				MultipartFile immagine = prodottoDTO.getImmagine();
+				Date dataCreazione = new Date();
+				String nomeFile = dataCreazione.getTime() + "_" + immagine.getOriginalFilename();
+				
+				try(InputStream inputStream = immagine.getInputStream()){
+					Files.copy(inputStream, Paths.get(percorsoCaricamento + nomeFile), StandardCopyOption.REPLACE_EXISTING);
+				}
+				
+				prodotto.setImmagine(nomeFile);
+			}
+			
+			
+			prodotto.setNome(prodottoDTO.getNome());
+			prodotto.setMarca(prodottoDTO.getMarca());
+			prodotto.setCategoria(prodottoDTO.getCategoria());
+			prodotto.setPrezzo(prodottoDTO.getPrezzo());
+			prodotto.setDescrizione(prodottoDTO.getDescrizione());
+			
+			
+			prodottoServ.aggiungiOModifica(prodotto);
+			
+			
+		}catch(Exception e) {
+			System.out.println("Exception"+ e.getMessage());
+		}
+		
+		
+		
+		return "redirect:/api/prodotti";
+	}
 }
--- src/main/java/com/generation/spring_bootthymeleaf/service/ProdottoServ.java
+++ src/main/java/com/generation/spring_bootthymeleaf/service/ProdottoServ.java
@@ -1,6 +1,7 @@
 package com.generation.spring_bootthymeleaf.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -28,6 +29,11 @@ public class ProdottoServ {
 		return null;
 	}
 	
+	
+	public Optional<Prodotto> ricercaProdotto(int idProdotto){
+		Optional<Prodotto> prodotto = prodottoRepo.findById(idProdotto);
+		return prodotto;
+	}
 	
 	
 }
