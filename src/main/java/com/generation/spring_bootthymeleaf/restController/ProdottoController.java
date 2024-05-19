4bf52b7f01d30d475fd1c9887c0945c97195c0f1
Mirko Di Franco <120678275+mirko-di-franco@users.noreply.github.com>
2024-05-19 17:12:54 +0200

    [add] logica service, logica controller e pagina lista prodotti

--- src/main/java/com/generation/spring_bootthymeleaf/restController/ProdottoController.java
+++ src/main/java/com/generation/spring_bootthymeleaf/restController/ProdottoController.java
@@ -1,16 +1,34 @@
 package com.generation.spring_bootthymeleaf.restController;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.generation.spring_bootthymeleaf.entity.Prodotto;
 import com.generation.spring_bootthymeleaf.service.ProdottoServ;
 
-@RestController
+@Controller
 @RequestMapping("api/prodotti")
 public class ProdottoController {
 	
 	@Autowired
 	ProdottoServ prodottoServ;
+	
+	
+	//significa che questa rotta funziona sia con "http://localhost:8080/api/prodotti" oppure "http://localhost:8080/api/prodotti/"
+	@GetMapping({"", "/"})
+	//il MODEL è un interfaccia di Spring che viene utilizzata per passare dati dal controller alla vista
+	public String listaProdotti(Model model) {
+		List<Prodotto> prodotti = prodottoServ.tuttiIprodotti();
+		//questo metodo aggiunge l'attributo `prodotti` al Model. il primo parametro è il nome con cui l'attributo sarà disponibile nella vista(in questo caso, `"prodotti"`),
+		  // e il secondo parametro è il valore effettivo dell'attributo(la lista dei prodotti a riga 26)
+		model.addAttribute("prodotti", prodotti);
+		//mi collego alla pagina index.html che si trova dentro la cartella template/prodotti
+		return "prodotti/index";
+	}
 
 }
--- src/main/java/com/generation/spring_bootthymeleaf/service/ProdottoServ.java
+++ src/main/java/com/generation/spring_bootthymeleaf/service/ProdottoServ.java
@@ -1,8 +1,11 @@
 package com.generation.spring_bootthymeleaf.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.generation.spring_bootthymeleaf.entity.Prodotto;
 import com.generation.spring_bootthymeleaf.repository.ProdottoRepo;
 
 @Service
@@ -10,4 +13,13 @@ public class ProdottoServ {
 
 	@Autowired
 	ProdottoRepo prodottoRepo;
+	
+	//METODO PER MOSTRARE TUTTI I PRODOTTI
+	public List<Prodotto> tuttiIprodotti(){
+		List<Prodotto> prodotti = prodottoRepo.findAll();
+		return prodotti;
+	}
+	
+	
+	
 }
